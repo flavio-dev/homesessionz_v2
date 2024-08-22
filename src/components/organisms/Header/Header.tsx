c6023d8c813463ae05c6791ef2910f80cd86f8e9
flavio <trash_flavio@hotmail.fr>
2024-08-22 16:53:28 +0200

    playing around

---
+++ src/components/organisms/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+
+// import Logo from 'components/Logo'
+// import SearchContainer from 'components/SearchContainer'
+
+import styles from "./Header.module.css";
+
+export const Header = () => (
+  <div className={styles.header}>
+    <div className={styles.headerInner}>
+      <div className={styles.headerPlaceholder}>
+        <Link href="/">høme sessiønz</Link>
+      </div>
+      <Link href="/">{/* <Logo /> */}</Link>
+      <div className={styles.headerSearch}>
+        green{/* <SearchContainer /> */}
+      </div>
+    </div>
+  </div>
+);
+
+export default Header;
