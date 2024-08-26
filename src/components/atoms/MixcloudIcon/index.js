8d37d36d2b7be08150cff33fb026a302f71bcffe
flavio <trash_flavio@hotmail.fr>
2024-08-26 13:31:51 +0200

    wrapping up the footer

---
+++ src/components/atoms/MixcloudIcon/index.js
@@ -0,0 +1,3 @@
+import MixcloudIcon from './MixcloudIcon'
+
+export default MixcloudIcon
--- src/components/organisms/Footer/Footer.tsx
+++ src/components/organisms/Footer/Footer.tsx
@@ -1,18 +1,42 @@
 import Link from "next/link";
 
 import styles from "./Footer.module.css";
+import thisYear from "@/utils/thisYear";
 
-// import InstaIcon from "components/InstaIcon";
-// import MixcloudIcon from "components/MixcloudIcon";
-// import HeartIcon from "components/HeartIcon";
-
-// import thisYear from "utils/thisYear";
+import HeartIcon from "@/components/atoms/HeartIcon";
+import InstaIcon from "@/components/atoms/InstaIcon";
+import MixcloudIcon from "@/components/atoms/MixcloudIcon";
 
 export const Footer = () => (
   <div className={styles.footer}>
-    <div className={styles.footerLinks}>
-      <Link href="/about">abøut</Link>
-      <Link href="/contact">cøntact</Link>
+    <div className={`default-wrapper-width ${styles.footerInner}`}>
+      <div className={styles.footerTopLine}>
+        <div className={styles.footerLinks}>
+          <Link href="/about">abøut</Link>
+          <Link href="/contact">cøntact</Link>
+        </div>
+        <div className={styles.footerMedia}>
+          <Link href="https://www.instagram.com/homesessionz/" target="_blank">
+            <InstaIcon />
+          </Link>
+          <Link href="https://www.mixcloud.com/homesessionz/" target="_blank">
+            <MixcloudIcon />
+          </Link>
+        </div>
+      </div>
+      <div
+        className={
+          false
+            ? `${styles.footerBottomLine} ${styles.footerBottomLineExtraSpace}`
+            : `${styles.footerBottomLine}`
+        }
+      >
+        <div className={styles.footerText}>
+          made with <HeartIcon /> by{" "}
+          <a href="https://github.com/flavio-dev">flavio-dev</a>
+        </div>
+        <div>copyrights {thisYear()}</div>
+      </div>
     </div>
   </div>
 );
